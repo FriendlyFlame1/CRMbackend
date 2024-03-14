57c1f4b424118908dd3799175f14c8a96f33b9ed
Friendlyflame1 <yash.goel@bntechnology.net>
2024-03-14 11:22:01 +0530

    changes

--- index.js
+++ index.js
@@ -92,5 +92,5 @@ app.put('/api/deals/:id', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
